a4121ecec7103ef1fa0ff6ca307cea6f5e78d8bd
Micaela Cembal <micaelacembal@gmail.com>
2025-06-11 18:38:06 -0300

    Implemtacion de IPersona en Persona

--- tp3/src/model/Persona.java
+++ tp3/src/model/Persona.java
@@ -1,14 +1,37 @@
 package model;
 
-public class Persona {
+import interfaces.IPersona;
+
+public class Persona implements IPersona {
     private String nombre;
-    private int id;
+    private int id;  // Se asume que "id" es el DNI
 
     public Persona(String nombre, int id) {
         this.nombre = nombre;
         this.id = id;
     }
 
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int getDni() {
+        return id;
+    }
+
+    @Override
+    public void setDni(int dni) {
+        this.id = dni;
+    }
+
+    @Override
     public String toString() {
         return nombre;
     }
