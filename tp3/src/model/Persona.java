9d50157532660ecc5fefee6b8aa5ebbb57188cbe
ariana progano <elmallian@gmail.com>
2025-06-11 18:33:48 -0300

    clase persona

--- tp3/src/model/Persona.java
+++ tp3/src/model/Persona.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Persona {
+    private String nombre;
+    private int id;
 
+    public Persona(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String toString() {
+        return nombre;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Persona)) return false;
+        Persona otra = (Persona) obj;
+        return this.id == otra.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
 }
